3376080acbfaf27c8fe42716a90898951b7aa06a
Pi Lanningham <pi.lanningham@gmail.com>
2023-06-04 13:03:29 -0400

    Remove unneccesary fields
    We'll put the dynamodb boilerplate stuff in a separate type in our internal repo

--- types/types.go
+++ types/types.go
@@ -30,35 +30,14 @@ type Delegation struct {
 }
 
 type Position struct {
-	TxHash      string
-	Index       int
-	CreatedSlot uint64
-
 	Owner string // TODO(pi): owner
 
 	Value      chainsync.Value
 	Delegation []Delegation
-
-	SpentTxHash string
-	SpentTxSlot uint64
-}
-
-type Witness struct {
-	PubKey    []byte
-	Signature []byte
 }
 
 type Earning struct {
 	Owner string
 	Date  time.Time
 	Value chainsync.Value
-
-	PaidTxId      string
-	PaidTx        []byte
-	PaidWitnesses []byte
-
-	TxValidTo   uint64
-	CooldownEnd uint64
-
-	SeenTxId string
 }
